89a299714adb8cb0b33a426a0d7cfb740a68edc1
Max Cantor <max@maxcantor.net>
2016-09-17 17:56:56 -0400

    Comments and refactoring.

--- script.js
+++ script.js
@@ -16,80 +16,34 @@ function build(tag, options) {
   return element;
 }
 
-var TooltipManager = {
-  element: document.getElementById("tooltip"),
-
-  watch: function(element, definition) {
-    element.addEventListener("mouseover", (event) => {
-      if(!event.firstCaughtBy) { event.firstCaughtBy = element; }
-      this.pushTooltip(event.firstCaughtBy, element, definition);
-    }, true);
-
-    element.addEventListener("mouseout", this.popTooltip.bind(this), true);
-  },
-
-  pushTooltip: function(outer, target, definition) {
-    this.element.appendChild(
-      build("div", {
-        className: "tooltip",
-        contains: [
-          build("div", {className: "word", contains: [
-            this.buildAlignedWord(outer, target)
-          ]}),
-          build("div", {className: "definition", text: definition})
-        ]
-      })
-    );
-  },
-
-  buildAlignedWord: function(outer, target) {
-    return this.deepCopy(outer, function(copy, original) {
-      if(original.nodeType === Node.ELEMENT_NODE) {
-        copy.classList.remove("word");
-      }
-      if(original === target) {
-        copy.className += " shown";
-      }
-    });
-  },
-
-  deepCopy: function(original, editClone) {
-    // copy classes, contents, IDs, but no children
-    var copy = original.cloneNode();
-    editClone(copy, original);
-    original.childNodes.forEach((child) => {
-      copy.appendChild(this.deepCopy(child, editClone));
-    });
-    return copy;
-  },
-
-  popTooltip: function(event) {
-    return this.element.removeChild(this.element.lastChild);
-  }
-};
-
-// A Slide contains one or more Cells, which helpful Tooltips are attached to.
-function Cell(selection, definition) {
-  this.word = selection.toString();
-  this.definition = definition;
-
-  this.element = build("span", {className: "word"});
-  NodeChirurgeon.wrapSelection(this.element, selection);
-  TooltipManager.watch(this.element, this.definition);
-}
+/*
+ * Slides
+ *
+ * Displays documents on the page.
+ *
+ * Call initialize() to get started.
+ *
+ **/
 
 var Slides = {
-  slides: data["slides"],
-  element: document.getElementById("slides"),
+  slides: [],
+  element: null,
 
   initialize: function() {
+    this.slides = data["slides"];
+    this.element = document.getElementById("slides");
+
     this.slides.forEach(function(slide) {
       var section = build("section", {className: "code"});
       slide.code.split("\n").forEach(function(line) {
         section.innerHTML += '<p class="line">' + line + '</p>';
       });
       this.element.appendChild(section);
+
     }.bind(this));
+
+    Editor.initialize();
+    TooltipManager.initialize();
   },
 
   appendControls: function(controls) {
@@ -97,55 +51,21 @@ var Slides = {
   }
 };
 
-// Split, enclose, and reattach nodes with surgical accuracy.
-var NodeChirurgeon = {
-  wrapSelection: function(wrapper, selection) {
-    var [start, end] = this.prepareBoundaries(selection);
 
-    start.parentNode.insertBefore(wrapper, start);
-
-    this.walkSiblings(start, end, (node) => wrapper.appendChild(node));
-  },
-
-  walkSiblings: function(start, end, callback) {
-    var cache, cursor = start;
-    do {
-      // cache the next sibling, since we're expecting to move
-      // the cursor node around within the DOM hierarchy
-      cache = cursor.nextSibling;
-      callback(cursor);
-      cursor = cache;
-    } while (cursor != end);
-  },
-
-  prepareBoundaries: function(selection) {
-    var start = this.prepare(selection.anchorNode, selection.anchorOffset);
-    var end = this.prepare(selection.focusNode, selection.focusOffset);
-    if(this.userDraggedRightToLeft(start, end)) {
-      return [end, start];
-    } else {
-      return [start, end];
-    }
-  },
-
-  userDraggedRightToLeft: function(a, b) {
-    // http://stackoverflow.com/a/23512678/16034
-    return a.compareDocumentPosition(b) === Node.DOCUMENT_POSITION_PRECEDING;
-  },
-
-  prepare: function(node, offset) {
-    if(offset > 0 && offset < node.length) {
-      // selected in the middle: Hello[, world!]
-      return node.splitText(offset);
-    } else {
-      // selected at a boundary: [Hello, world!]
-      return node;
-    }
-  }
-};
+/*
+ * Editor
+ *
+ * Enable live creation of new definition Cells.
+ *
+ **/
 
 var Editor = {
-  addDefinition: function() {
+  initialize: function() {
+    this.buildControls();
+    Slides.appendControls(this.defControls);
+  },
+
+  addDefinition: function(event) {
     var cell = new Cell(
       document.getSelection(),
       this.defInput.value
@@ -167,13 +87,201 @@ var Editor = {
                          id: "definition-controls",
                          contains: [this.defInput, this.defButton]
                        });
-  },
-
-  activate: function() {
-    this.buildControls();
-    Slides.appendControls(this.defControls);
   }
 };
 
-Slides.initialize();
-Editor.activate();
+
+/*
+ * Cell
+ *
+ * A chunk of text in a Slide that produce helpful Tooltips when hovered over.
+ *
+ **/
+
+function Cell(selection, definition) {
+  this.word = selection.toString();
+  this.definition = definition;
+
+  this.element = build("span", {className: "word"});
+
+  NodeChirurgeon.wrapSelection(this.element, selection);
+  TooltipManager.watch(this.element, this.definition);
+}
+
+
+/*
+ * TooltipManager
+ *
+ * Given an element and a definition, make a hover popup for the element.
+ *
+ * By passing true to Element.addEventListener(), we cause element to listen
+ * in "capture" mode, which means the callback will be triggered as the event
+ * descends down the hierarchy rather than on its way back up.
+ *
+ * console.log(generateMessage(data));  |  1  <- firstCaughtBy
+ *             generateMessage(data)    |  2
+ *                             data     V  3
+ *
+ * This allows us to generate tooltips in order "on the way down", without
+ * ever having to keep track of the tooltips ourselves.
+ *
+ **/
+
+var TooltipManager = {
+  element: null,
+
+  initialize: function() {
+    this.element = document.getElementById("tooltip");
+  },
+
+  watch: function(element, definition) {
+    element.addEventListener("mouseover", (event) => {
+      if(!event.firstCaughtBy) { event.firstCaughtBy = element; }
+      this.pushTooltip(
+        AlignedWord.buildElement(event.firstCaughtBy, element),
+        definition
+      );
+    }, true);
+
+    element.addEventListener("mouseout", this.popTooltip.bind(this), true);
+  },
+
+  pushTooltip: function(elWord, definition) {
+    this.element.appendChild(
+      build("div", {
+        className: "tooltip",
+        contains: [
+          build("div", {className: "word", contains: [elWord]}),
+          build("div", {className: "definition", text: definition})
+        ]
+      })
+    );
+  },
+
+  popTooltip: function(event) {
+    return this.element.removeChild(this.element.lastChild);
+  }
+};
+
+
+/*
+ * AlignedWord
+ *
+ * Creates horizontally aligned nested Tooltips by reproducing their entire
+ * node tree ancestry. For example:
+ *
+ * ACTUAL TOOLTIP CONTENTS:
+ *
+ * console.log(generateMessage(data));    <-- outer
+ * console.log(generateMessage(data));
+ * console.log(generateMessage(data));    <-- target
+ *
+ * But each Cell gets its own element, and we can use color: transparent
+ * to hide the text of ancestors in the Tooltip for a given Cell, so the
+ * Tooltips end up looking like this:
+ *
+ * APPARENT TOOLTIP CONTENTS:
+ *
+ * console.log(generateMessage(data));    <-- outer
+ *             generateMessage(data)
+ *                             data       <-- target
+ **/
+
+var AlignedWord = {
+  buildElement: function(outer, target) {
+    return deepCopy(outer, function(copy, original) {
+      if(original === target) {
+        copy.className += " shown";
+      }
+    });
+  }
+};
+
+function deepCopy(original, editClone) {
+  // copy classes, IDs, but not child text or elements
+  var copy = original.cloneNode();
+  editClone(copy, original);
+  original.childNodes.forEach(function(child) {
+    copy.appendChild(deepCopy(child, editClone));
+  });
+  return copy;
+}
+
+
+/*
+ * NodeChirurgeon
+ *
+ * Divide, extract, enclose, and transplant nodes with surgical accuracy.
+ *
+ * Given the following selection on a <p> with one text node child,
+ * and a wrapper <span></span>:
+ *                  ___________
+ * <p> "console.log(myAdminCred);" </p>
+ *
+ * The Chirurgeon will produce:
+ *
+ * <p>
+ *     "console.log("
+ *     <span>
+ *         "myAdminCred"
+ *     </span>
+ *     ");"
+ * </p>
+ *
+ **/
+
+var NodeChirurgeon = {
+  wrapSelection: function(wrapper, selection) {
+    var [start, end] = Scalpel.makeBoundaryIncisions(selection);
+    Suture.transplantNodes(wrapper, start, end);
+  }
+};
+
+var Scalpel = {
+  makeBoundaryIncisions: function(selection) {
+    var start = this.cut(selection.anchorNode, selection.anchorOffset);
+    var end = this.cut(selection.focusNode, selection.focusOffset);
+    if(this.userDraggedRightToLeft(start, end)) {
+      return [end, start];
+    } else {
+      return [start, end];
+    }
+  },
+
+  cut: function(node, cutIndex) {
+    if(cutIndex > 0 && cutIndex < node.length) {
+      // selected in the middle: Hello[, world!]
+      return node.splitText(cutIndex);
+    } else {
+      // selected at a boundary: [Hello, world!]
+      return node;
+    }
+  },
+
+  userDraggedRightToLeft: function(a, b) {
+    // http://stackoverflow.com/a/23512678/16034
+    return a.compareDocumentPosition(b) === Node.DOCUMENT_POSITION_PRECEDING;
+  }
+};
+
+var Suture = {
+  transplantNodes: function(newParent, start, end) {
+    // Insert the new parent first, before we mess with the DOM.
+    start.parentNode.insertBefore(newParent, start);
+    // Transplant all siblings between start & end to the new parent.
+    this.walkSiblings(start, end, (node) => newParent.appendChild(node));
+  },
+
+  walkSiblings: function(start, end, callback) {
+    var cache, cursor = start;
+    do {
+      // cache the next sibling, since we're expecting to move
+      // the cursor node around within the DOM hierarchy
+      cache = cursor.nextSibling;
+      callback(cursor);
+      cursor = cache;
+    } while (cursor != end);
+  }
+};
+
+// Initializers are called in the document.
