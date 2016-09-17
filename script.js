1690172bb8d29bf8a188d52ce17d67c5a4323510
Max Cantor <max@maxcantor.net>
2016-09-16 20:05:54 -0400

    Start building dynamic editor.

--- script.js
+++ script.js
@@ -9,46 +9,83 @@ function createElement(tag, className, tap) {
   return el;
 }
 
-function createShowMetaListener(element, word, definition) {
+function createTooltipListeners(element, wordTextNode, definition) {
+  var tipWord = wordTextNode.cloneNode();
+  var tipDef = document.createTextNode(definition);
   element.addEventListener("mouseover", function() {
-    elDefinition.appendChild(document.createTextNode(definition));
-    elWord.appendChild(document.createTextNode(word));
+    elWord.appendChild(tipWord);
+    elDefinition.appendChild(tipDef);
   });
-}
-
-function createHideMetaListener(element) {
   element.addEventListener("mouseout", function() {
-    elDefinition.innerHTML = "";
-    elWord.innerHTML = "";
+    elWord.removeChild(tipWord);
+    elDefinition.removeChild(tipDef);
   });
 }
 
-function createHoverWord(word, definitionKey) {
+function createHoverWord(wordTextNode, definition) {
   return createElement("span", "word", function(span) {
-    span.textContent = word;
-    span.className += " " + definitionKey;
-    createShowMetaListener(span, word, data["definitions"][definitionKey]);
-    createHideMetaListener(span);
-  });
-}
-
-function attachSemicolon(elSlide) {
-  elSlide.lastChild.className += " precedes-a-semicolon";
-  elSlide.appendChild(createHoverWord(";", "semicolon"));
-}
-
-function generateSlide(slide) {
-  return createElement("div", "slide", function(elDiv) {
-    slide["words"].forEach(function(word) {
-      elDiv.appendChild(createHoverWord(word.text, word.key));
-    });
-
-    if(slide.semi) {
-      attachSemicolon(elDiv);
-    }
+    span.appendChild(wordTextNode);
+    createTooltipListeners(span, wordTextNode, definition);
   });
 }
 
 data["slides"].forEach(function(slide) {
-  elSlides.appendChild(generateSlide(slide));
+  createElement("section", "code", function(section) {
+    slide.code.split("\n").forEach(function(line) {
+      section.innerHTML += '<p class="line">' + line + '</p>';
+    });
+    elSlides.appendChild(section);
+  });
 });
+
+var Editor = {
+  makeElement: function(tag, options) {
+    var el = document.createElement(tag);
+    if(options.id) { el.id = options.id; }
+    if(options.contains) {
+      options.contains.forEach(function(child) {
+        el.appendChild(child);
+      });
+    }
+    if(options.text) { el.textContent = options.text; }
+    if(options.onclick) { el.onclick = options.onclick.bind(this); }
+    return el;
+  },
+
+  except: function(key) {
+    console.log("Exception: %s", key);
+  },
+
+  addDefinition: function() {
+    var selection = document.getSelection();
+    if(selection.toString().length == 0) { this.except("no selection"); }
+    var range = selection.getRangeAt(0).cloneRange();
+    var start = selection.anchorOffset;
+    var end = selection.focusOffset - start;
+    var nodeBefore = selection.anchorNode;
+    var line = nodeBefore.parentNode;
+    var selectedTextNode = nodeBefore.splitText(start);
+    var nodeAfter = selectedTextNode.splitText(end);
+    var definition = this.defInput.value;
+    if(definition.length == 0) { this.except("no definition"); }
+    var newElement = createHoverWord(selectedTextNode, definition);
+    line.insertBefore(newElement, nodeAfter);
+  },
+
+  activate: function() {
+    this.defButton = this.makeElement("button", {
+      id: "add-definition",
+      text: "Define",
+      onclick: this.addDefinition
+    });
+    this.defInput = this.makeElement("input", {id: "write-definition"});
+    this.definitionControls = this.makeElement("div", {
+      id: "definition-controls",
+      contains: [this.defInput, this.defButton]
+    });
+
+    elDefinition.appendChild(this.definitionControls);
+  }
+};
+
+Editor.activate();
